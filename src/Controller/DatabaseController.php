10a61351930677e98c44b483e0e889f152ce5124
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2022-03-27 16:43:54 +0100

    feat: add database use case

--- src/Controller/DatabaseController.php
+++ src/Controller/DatabaseController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller;
 
+use App\Form\Type\ConnectionType;
+use App\Model\Database\Entities\ConnectionEntity;
+use App\Model\Database\Services\DatabaseService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DatabaseController extends AbstractController
 {
+
     /**
      * @Route("/", name="database_index")
      */
     public function index(): Response
     {
         return $this->render('database/index.html.twig');
+    }
+
+    /**
+     * @Route("/database/{id}/edit", name="database_post_edit")
+     */
+    public function edit(Request $request, string $id, DatabaseService $databaseService): Response
+    {
+        // showing off how you could actually avoid creating the Form object on GET
+        // $form = null;
+
+        $connectionEntity = $databaseService->getSqlCe();
+
+        if ($id === 'mysqlServer') {
+            $connectionEntity = $databaseService->getMysqlCe();
+        }
+
+        // if ($request->isMethod('POST')) {
+        $form = $this->createForm(ConnectionType::class, $connectionEntity);
+        $form->handleRequest($request);
+        // }
+
+        if ($form && $form->isSubmitted() && $form->isValid()) {
+            $connectionEntity = $form->getData();
+            $databaseService->persist($connectionEntity);
+            return $this->redirectToRoute('database_index');
+        }
+
+        return $this->renderForm('database/edit.html.twig', [
+            'connectionEntity' => $connectionEntity,
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Form/Type/ConnectionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Model\Database\Entities\ConnectionEntity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConnectionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('user', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('address', TextType::class)
+            ->add('database', TextType::class)
+            ->add('exposedPort', NumberType::class)
+            ->add('edit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ConnectionEntity::class,
+        ]);
+    }
+}
