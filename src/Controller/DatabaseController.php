5b5faeb6774f2ff43135a51f15a86bec68c6f130
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2022-04-11 23:52:16 +0100

    feat: add new structure logic to work with the new connections

--- src/Controller/DatabaseController.php
+++ src/Controller/DatabaseController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
-use App\Form\ConnectionType;
+use App\Form\NmConnectionModel;
+use App\Form\NmConnectionType;
+use App\Form\UwConnectionModel;
+use App\Form\UwConnectionType;
 use App\Model\Database\DatabaseService;
+use App\Repository\NmConnectionRepository;
+use App\Repository\UwConnectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,27 +28,67 @@ class DatabaseController extends AbstractController
     /**
      * @Route("/database/{id}/edit", name="database_post_edit")
      */
-    public function edit(Request $request, string $id, DatabaseService $databaseService): Response
-    {
-        $connectionEntity = $databaseService->getSqlCe();
+    public function edit(
+        Request $request,
+        string $id,
+        DatabaseService $databaseService,
+        NmConnectionRepository $nmConnectionRepository,
+        UwConnectionRepository $uWconnectionRepository
+    ): Response {
+        $connectionModel = new UwConnectionModel();
+        $uWconnectionEntity = $uWconnectionRepository->get();
+        $connectionModel->setData($uWconnectionEntity);
+        $form = $this->createForm(UwConnectionType::class, $connectionModel);
 
         if ($id === 'mysqlServer') {
-            $connectionEntity = $databaseService->getMysqlCe();
+            $connectionModel = new NmConnectionModel();
+            $nmMysqlConnectionEntity = $nmConnectionRepository->getByType('mysqlServer');
+            $connectionModel->setData($nmMysqlConnectionEntity);
+            $form = $this->createForm(NmConnectionType::class, $connectionModel, [
+                'type' => $id
+            ]);
         }
 
-        // if ($request->isMethod('POST')) {
-        $form = $this->createForm(ConnectionType::class, $connectionEntity);
+        if ($id === 'nmSqlServer') {
+            $connectionModel = new NmConnectionModel();
+            $nmSqlConnectionEntity = $nmConnectionRepository->getByType('sqlServer');
+            $connectionModel->setData($nmSqlConnectionEntity);
+            $form = $this->createForm(NmConnectionType::class, $connectionModel, [
+                'type' => $id
+            ]);
+        }
+
         $form->handleRequest($request);
+
+        // if ($request->isMethod('POST')) {
+        // $form = $this->createForm(NmConnectionType::class, $connectionModel);
+        // $form->handleRequest($request);
         // }
 
         if ($form && $form->isSubmitted() && $form->isValid()) {
-            $connectionEntity = $form->getData();
-            $databaseService->persist($connectionEntity);
+            $connectionModel = $form->getData();
+            $data = $connectionModel->toArray();
+            $authentication = $form->get('authentication')->getData();
+            $databaseService->persist($data, $authentication);
             return $this->redirectToRoute('database_index');
         }
 
-        return $this->renderForm('database/edit.html.twig', [
+        if ($id === 'mysqlServer' | $id === 'nmSqlServer') {
+            return $this->renderForm('database/edit.html.twig', [
+                'form' => $form,
+            ]);
+        }
+
+        return $this->renderForm('database/uw_edit.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    /**
+     * @Route("/database/options", name="database_authentication_options", methods={"GET"})
+     */
+    public function listAuthenticationOptions()
+    {
+        return $this->render('database/authentication_options.html.twig');
     }
 }
