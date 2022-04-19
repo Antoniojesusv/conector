3ba3d6bd53f9824581f272773f5884533eaee3e7
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2022-04-19 22:17:52 +0100

    feat: add Synchronisation controller

--- src/Controller/SynchronisationController.php
+++ src/Controller/SynchronisationController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SynchroniseController extends AbstractController
+class SynchronisationController extends AbstractController
 {
     /**
-     * @Route("/synchronise", name="synchronise_index")
+     * @Route("/synchronisation", name="synchronisation_index")
      */
     public function index(): Response
     {
-        return $this->render('synchronise/index.html.twig');
+        return $this->render('synchronisation/index.html.twig');
     }
 }
