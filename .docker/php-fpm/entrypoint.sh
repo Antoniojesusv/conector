0d05b9c2e4f4fd2f823878d633d8776acddc4564
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2022-05-07 09:40:37 +0100

    build: add new eurowin image folder

--- .docker/php-fpm/entrypoint.sh
+++ .docker/php-fpm/entrypoint.sh
@@ -5,6 +5,7 @@ WORKDIR="/var/www"
 chown -R $USER:www-data $WORKDIR/.env
 chown -R $USER:www-data $WORKDIR/public/temporaryImages
 chown -R $USER:www-data $WORKDIR/Eurowin/fotoseurowin
+chown -R $USER:www-data $WORKDIR/fotoseurowin
 chown -R $USER:www-data $WORKDIR/images-log.json
 chown -R $USER:www-data $WORKDIR/articles-log.json
 
