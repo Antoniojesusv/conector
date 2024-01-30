f9a3869f768d44b02547d8999167104d3e317115
Antonio Jesús Vázquez Muñoz <ajvazquez@ext.elconfidencial.com>
2024-01-30 12:44:01 +0100

    build(env): create env local file

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -6,9 +6,4 @@ echo $HOST_DEVICE_IP
 grep '^xdebug.client_host ' ./.docker/php/xdebug.ini
 sed -i "s,^xdebug.client_host =.*$,xdebug.client_host = ${HOST_DEVICE_IP}," ./.docker/php/xdebug.ini
 
-grep '^HOST_DEVICE_IP' ./.env
-sed -i "s,^HOST_DEVICE_IP=.*$,HOST_DEVICE_IP=${HOST_DEVICE_IP}," ./.env
-
 docker-compose up --build -d
-
-# HOST_DEVICE_IP=$HOST_DEVICE_IP docker-compose up --build -d
